89ced8fd2171b023106e02edeca6fb976aaa9467
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-05-21 00:30:33 +0600

    payment

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,6 +34,38 @@ function verifyJWT(req, res, next) {
   });
 }
 
+//Payment Confirmation Email
+// function sendPaymentConfirmationEmail(booking) {
+//   const { patient, patientName, treatment, date, slot } = booking;
+
+//   var email = {
+//     from: process.env.EMAIL_SENDER,
+//     to: patient,
+//     subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+//     text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+//     html: `
+//       <div>
+//         <p> Hello ${patientName}, </p>
+//         <h3>Your Appointment for ${treatment} is confirmed</h3>
+//         <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+
+//         <h3>Our Address</h3>
+//         <p>Andor Killa Bandorban</p>
+//         <p>Bangladesh</p>
+//         <a href="https://web.programming-hero.com/">unsubscribe</a>
+//       </div>
+//     `,
+//   };
+
+//   emailClient.sendMail(email, function (err, info) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Message sent: ", info);
+//     }
+//   });
+// }
+
 async function run() {
   try {
     await client.connect();
@@ -43,6 +76,42 @@ async function run() {
       .db("doctors_portal")
       .collection("bookings");
     const userCollection = client.db("doctors_portal").collection("users");
+    const paymentsCollection = client
+      .db("doctors_portal")
+      .collection("payments");
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.patch("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const result = await paymentsCollection.insertOne(payment);
+      const updatedBooking = await bookingCollection.updateOne(
+        filter,
+        updateDoc
+      );
+      res.send(updateDoc);
+    });
+    app.get;
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -116,6 +185,13 @@ async function run() {
       } else {
         return res.status(403).send({ message: "Forbidden Access" });
       }
+    });
+
+    app.get("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
     });
 
     app.post("/booking", async (req, res) => {
